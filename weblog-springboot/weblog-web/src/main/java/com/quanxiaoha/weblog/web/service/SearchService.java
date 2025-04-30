a9b0da34e6267d73b5975c0fa248d680224cbe25
史灵岳 <2334713605@qq.com>
2025-04-30 16:37:20 +0800

    2.2

---
+++ weblog-springboot/weblog-web/src/main/java/com/quanxiaoha/weblog/web/service/SearchService.java
@@ -0,0 +1,15 @@
+package com.quanxiaoha.weblog.web.service;
+
+import com.quanxiaoha.weblog.common.utils.Response;
+import com.quanxiaoha.weblog.web.model.vo.search.SearchArticlePageListReqVO;
+
+public interface SearchService {
+
+    /**
+     * 关键词分页搜索
+     * @param searchArticlePageListReqVO
+     * @return
+     */
+    Response searchArticlePageList(SearchArticlePageListReqVO searchArticlePageListReqVO);
+}
+
